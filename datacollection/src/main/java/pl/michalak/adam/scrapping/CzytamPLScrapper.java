9d271425fa1ea3c289a6d1154cbdcb0803a065cc
Adam Michalak <adam_michalak@epam.com>
2019-02-25 10:02:48 +0100

    read book data from all pages

--- datacollection/src/main/java/pl/michalak/adam/scrapping/CzytamPLScrapper.java
+++ datacollection/src/main/java/pl/michalak/adam/scrapping/CzytamPLScrapper.java
@@ -9,27 +9,36 @@ import java.util.Set;
 
 class CzytamPLScrapper implements PageScrapper {
 	private ScrapperAPI scrapper;
+	private int pageNumber;
 
 	CzytamPLScrapper(ScrapperAPI scrapper){
 		this.scrapper = scrapper;
+		this.pageNumber = 0;
 	}
 
 	@Override
 	public Set<ScrappedBook> scrapData() {
 		Set<ScrappedBook> scrappedBooks = new HashSet<>();
-
-		try {
-			scrapper.connect(CzytamPLQueries.URL.getQuery());
-		} catch (IOException e) {
-			System.err.println("Could not connect to "+TaniaKsiazkaQueries.URL);
-		}
-		for (Row row : scrapper.getTableRows(CzytamPLQueries.TABLE.getQuery())) {
-			String title = scrapper.getTitle(CzytamPLQueries.TITLEROW.getQuery(), row);
-			String author = scrapper.getAuthor(CzytamPLQueries.AUTHORROW.getQuery(), row);
-			double price = DataFormattingHelper.parseStringToDouble(scrapper.getPrice(CzytamPLQueries.PRICEROW.getQuery(), row).substring(9));
-			double previousPrice = DataFormattingHelper.parseStringToDouble(scrapper.getPromoDetails(CzytamPLQueries.PROMODETAILSROW.getQuery(), row));
-			scrappedBooks.add(ScrappedBook.BookBuilder.create(title).setAuthor(author).setPrice(price).setPromoDetails(DataFormattingHelper.getPromoInPercents(price, previousPrice)).build());
+		while(pageNumber < 10) { //as of February 2019 there are 7 pages and accessing page number 8 returns responseCode 200 -> safe solution.
+			pageNumber++;
+			try {
+				scrapper.connect(getURLOfNextPage());
+			} catch (IOException e) {
+				System.err.println("Could not connect to " + TaniaKsiazkaQueries.URL);
+			}
+			for (Row row : scrapper.getTableRows(CzytamPLQueries.TABLE.getQuery())) {
+				String title = scrapper.getTitle(CzytamPLQueries.TITLEROW.getQuery(), row);
+				String author = scrapper.getAuthor(CzytamPLQueries.AUTHORROW.getQuery(), row);
+				double price = DataFormattingHelper.parseStringToDouble(scrapper.getPrice(CzytamPLQueries.PRICEROW.getQuery(), row).substring(9));
+				double previousPrice = DataFormattingHelper.parseStringToDouble(scrapper.getPromoDetails(CzytamPLQueries.PROMODETAILSROW.getQuery(), row));
+				scrappedBooks.add(ScrappedBook.BookBuilder.create(title).setAuthor(author).setPrice(price).setPromoDetails(DataFormattingHelper.getPromoInPercents(price, previousPrice)).build());
+			}
 		}
 		return scrappedBooks;
+	}
+
+
+	private String getURLOfNextPage(){
+		return CzytamPLQueries.URL.getQuery().replace("25642-1", "25642-"+pageNumber);
 	}
 }
--- datacollection/src/main/java/pl/michalak/adam/scrapping/TaniaKsiazkaScrapper.java
+++ datacollection/src/main/java/pl/michalak/adam/scrapping/TaniaKsiazkaScrapper.java
@@ -10,26 +10,35 @@ import java.util.Set;
 class TaniaKsiazkaScrapper implements PageScrapper {
 
 	private ScrapperAPI scrapper;
+	private int pageNumber;
 
 	TaniaKsiazkaScrapper(ScrapperAPI scrapper){
 		this.scrapper = scrapper;
+		this.pageNumber = 0;
 	}
 
 	@Override
 	public Set<ScrappedBook> scrapData() {
 		Set<ScrappedBook> scrappedBooks = new HashSet<>();
-		try {
-			scrapper.connect(TaniaKsiazkaQueries.URL.getQuery());
-		} catch (IOException e) {
-			System.err.println("Could not connect to "+TaniaKsiazkaQueries.URL);
-		}
-		for (Row row : scrapper.getTableRows(TaniaKsiazkaQueries.TABLE.getQuery())) {
-			String title = scrapper.getTitle(TaniaKsiazkaQueries.TITLEROW.getQuery(), row);
-			String author = scrapper.getAuthor(TaniaKsiazkaQueries.AUTHORROW.getQuery(), row);
-			double price = DataFormattingHelper.parseStringToDouble(scrapper.getPrice(TaniaKsiazkaQueries.PRICEROW.getQuery(), row));
-			String promoDetails = scrapper.getPromoDetails(TaniaKsiazkaQueries.PROMODETAILSROW.getQuery(), row);
-			scrappedBooks.add(ScrappedBook.BookBuilder.create(title).setAuthor(author).setPrice(price).setPromoDetails(promoDetails).build());
+		while(pageNumber < 1500) { //as of February 2019 there are 1497 pages and accessing page number 1498 returns redirects back to page 1497 -> safe solution.
+			pageNumber++;
+			try {
+				scrapper.connect(getURLOfNextPage());
+			} catch (IOException e) {
+				System.err.println("Could not connect to " + TaniaKsiazkaQueries.URL);
+			}
+			for (Row row : scrapper.getTableRows(TaniaKsiazkaQueries.TABLE.getQuery())) {
+				String title = scrapper.getTitle(TaniaKsiazkaQueries.TITLEROW.getQuery(), row);
+				String author = scrapper.getAuthor(TaniaKsiazkaQueries.AUTHORROW.getQuery(), row);
+				double price = DataFormattingHelper.parseStringToDouble(scrapper.getPrice(TaniaKsiazkaQueries.PRICEROW.getQuery(), row));
+				String promoDetails = scrapper.getPromoDetails(TaniaKsiazkaQueries.PROMODETAILSROW.getQuery(), row);
+				scrappedBooks.add(ScrappedBook.BookBuilder.create(title).setAuthor(author).setPrice(price).setPromoDetails(promoDetails).build());
+			}
 		}
 		return scrappedBooks;
+	}
+
+	private String getURLOfNextPage(){
+		return new StringBuffer().append(TaniaKsiazkaQueries.URL.getQuery()).append("/page-").append(pageNumber).toString();
 	}
 }
